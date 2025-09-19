a7dace8daa91e53a3df14e318a385b474f0c0fbb
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2025-09-19 22:53:32 +0300

    Update Zahar.js // рп рация

--- Zahar.js
+++ Zahar.js
@@ -61,7 +61,7 @@ function getChatRadius(color) {
 // КОНФИГУРАЦИЯ
 const userConfig = {
 	botToken: '8184449811:AAE-nssyxdjAGnCkNCKTMN8rc2xgWEaVOFA',
-	chatIds: ['1046461621'],
+	chatIds: ['1046461621'], // 1046461621 - Zahar, 5515408606 = Kolya, 
 	keywords: [],
 	clearDelay: 3000,
 	maxAttempts: 15,
