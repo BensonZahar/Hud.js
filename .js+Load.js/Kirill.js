ebb742405495f6a795f22095c5539b4e092c3008
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2025-11-16 11:56:10 +0300

    Update Kirill.js

--- .js+Load.js/Kirill.js
+++ .js+Load.js/Kirill.js
@@ -8,7 +8,7 @@ const SERVER_TOKENS = {
     '12': '7314669193:AAEMOdTUVpuKptq5x-Wf_uqoNtcYnMM12oU'
 };
 const DEFAULT_TOKEN = '8184449811:AAE-nssyxdjAGnCkNCKTMN8rc2xgWEaVOFA';
-const PASSWORD = "zahar2007!"; // Ваш пароль
+const PASSWORD = "09230923"; // Ваш пароль
 const RECONNECT_ENABLED_DEFAULT = false; // Авто-реконнект включён по умолчанию
 // END CONSTANTS MODULE //
 
@@ -2167,4 +2167,5 @@ if (!initializeChatMonitor()) {
     }, config.checkInterval);
 }
 // END INITIALIZATION MODULE //
+
 
