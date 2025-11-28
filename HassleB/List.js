d0fe363ba7049c8122e8f3bddf32529842c0b196
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2025-11-28 14:52:51 +0300

    Add files via upload

---
+++ HassleB/List.js
@@ -0,0 +1,22 @@
+// List.js - Централизованные конфигурации
+const USER_CONFIGS = {
+    'Zahar': {
+        CHAT_IDS: ['-1003040555627'],
+        PASSWORD: 'zahar2007',
+        RECONNECT_ENABLED_DEFAULT: true
+    },
+    'Kirill': {
+        CHAT_IDS: ['-1003202329790'],
+        PASSWORD: '09230923',
+        RECONNECT_ENABLED_DEFAULT: true
+    },
+    'Kolya': {
+        CHAT_IDS: ['-1003102212423'],
+        PASSWORD: 'kol16052011',
+        RECONNECT_ENABLED_DEFAULT: false
+    }
+};
+
+// Экспортируем в глобальную область
+window.USER_CONFIGS = USER_CONFIGS;
+console.log('[List.js] Конфигурации пользователей загружены');
