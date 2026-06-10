c5aa6bd3699a5ca8516d68c1c1500c10ee80acb1
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-06-10 16:34:10 +0300

    Update List.js

--- HassleB/List.js
+++ HassleB/List.js
@@ -7,10 +7,10 @@ const USER_CONFIGS = {
         RECONNECT_ENABLED_DEFAULT: true,
         // Токены Telegram-ботов для аккаунтов Захара (1–8)
         BOT_TOKENS: {
-            '1': '8335162903:AAEIIBJJwJybcfk3sZBrMhv3hWm2u4FeRHY',
-            '2': '8689391102:AAFoas9FUjBhPwaJsJ9yJixzTAJ2Bjhom5I',
-            '3': '8641308857:AAGyBkHJu3OGLiDZD8bXbIfFJuOEh2_dz-Y',
-            '4': '8437476753:AAHiWXIFByf2s2TCO7JbKVj4LMKO_plrAKk',
+            '1': '8641308857:AAGyBkHJu3OGLiDZD8bXbIfFJuOEh2_dz-Y',
+            '2': '8335162903:AAEIIBJJwJybcfk3sZBrMhv3hWm2u4FeRHY',
+            '3': '8549354393:AAH3KUXtuSBZJ4SO4qw5s5WmWJ9_kypclBY',
+            '4': '7314669193:AAEMOdTUVpuKptq5x-Wf_uqoNtcYnMM12oU', // новые
             '5': '8709864021:AAHCs8QCioLBWfUJLyqxVuS_TCqDIbc3Naw',
             '6': '8692734140:AAEHthG-PpPOoY_0qmG25bzHafLIlcErs3M',
             '7': '8770179898:AAEm93rbx8VGvuTJJX0grD47WSQf1LCvTcI',
