1907437f4a002e60385ddd5f237705f1772e6491
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-06-26 12:55:49 +0300

    Update List.js вернул

--- HassleB/List.js
+++ HassleB/List.js
@@ -5,23 +5,17 @@ const USER_CONFIGS = {
         CHAT_IDS: ['-1003040555627'],
         PASSWORD: 'zahar2007',
         RECONNECT_ENABLED_DEFAULT: true,
-        // Токены Telegram-ботов для аккаунтов Захара (1–8)
         BOT_TOKENS: {
-            '1': '8512909288:AAEoTnIgdkvmrZ6DIVEgVFnG97tOzQQK3KU',
-            '2': '8335162903:AAEIIBJJwJybcfk3sZBrMhv3hWm2u4FeRHY',
-            '3': '8549354393:AAH3KUXtuSBZJ4SO4qw5s5WmWJ9_kypclBY',
-            '4': '7314669193:AAEMOdTUVpuKptq5x-Wf_uqoNtcYnMM12oU', // новые
-            '5': '8709864021:AAHCs8QCioLBWfUJLyqxVuS_TCqDIbc3Naw',
-            '6': '8692734140:AAEHthG-PpPOoY_0qmG25bzHafLIlcErs3M',
-            '7': '8770179898:AAEm93rbx8VGvuTJJX0grD47WSQf1LCvTcI',
-            '8': '8789490440:AAHpJUJ4650E2SP62bDn7GEVggD9fiIYh54'
+            '1': '8512909288:AAFlMnAVAHTLBWWnhI2pM6sxzFkUuEqWzJs', // @hasslep_bot
+            '2': '8335162903:AAGa7TwdKg2BJQy4EocwUNV09lP78mv8hS4', // @hacc01_bot
+            '3': '8549354393:AAGl3oXMVqbaChIkhbD-lQANeDpBx450-8Y', // @hassleb9_bot
+            '4': '7314669193:AAEv8n9DBy5dt8sgIPT-PMwQc3VwtnBwcWw'  // @hassleb12_bot
         }
     },
     'Kolya': {
         CHAT_IDS: ['-1003102212423'],
         PASSWORD: 'kol16052011',
         RECONNECT_ENABLED_DEFAULT: true,
-        // Токены Telegram-ботов для аккаунтов Коли (вставь свои)
         BOT_TOKENS: {
             '1': '8496708572:AAHpNdpNEAQs9ecdosZn3sCsQqJhWdLRn7U',
             '2': '7088892553:AAEQiujKWYXpH16m0L-KijpKXRT-i4UIoPE'
@@ -29,6 +23,5 @@ const USER_CONFIGS = {
     }
 };
 
-// Экспортируем в глобальную область
 window.USER_CONFIGS = USER_CONFIGS;
 console.log('[List.js] Конфигурации пользователей загружены');
