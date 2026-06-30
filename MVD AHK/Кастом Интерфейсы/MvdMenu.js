78341f542f4bbe455a16835cb8defd33028a1ed8
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-06-30 10:30:09 +0300

    Update MvdMenu.js

--- MVD AHK/Кастом Интерфейсы/MvdMenu.js
+++ MVD AHK/Кастом Интерфейсы/MvdMenu.js
@@ -297,7 +297,7 @@ const _sfc_main={
         isPinkNick(){
             try{
                 const ownNick=window.App?.$store?.getters?.['player/nickName'];
-                return ownNick==="Aleksandr_Alekseevih";
+                return ownNick==="Fura_Loidov";
             }catch(e){ return false; }
         },
         pinkNickName(){
