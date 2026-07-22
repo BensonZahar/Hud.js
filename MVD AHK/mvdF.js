3d3b5e06d93280b21b3c6896775c2e68229dca49
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-07-22 22:33:39 +0300

    Update mvdF.js

--- MVD AHK/mvdF.js
+++ MVD AHK/mvdF.js
@@ -218,7 +218,7 @@ function _showAccessDenied(nick) {
 // ── ВСЁ ЧТО НИЖЕ ВЫПОЛНЯЕТСЯ ТОЛЬКО ЕСЛИ НИК ПРОШЁЛ ПРОВЕРКУ ──
 
 // MVD AHK VERSION: 2.3 (NAPARNICK)
-console.log("[INIT] === MVD AHK v4.2 ЗАГРУЖЕН ===");
+console.log("[INIT] === MVD AHK v4.444 ЗАГРУЖЕН ===");
 // ── Авто-обновление собственного ID (каждые 30 секунд) ──
 // Гарантирует что hud.info.id всегда актуальный, даже без /has
 setInterval(function() {
