134880057cfdd4c6aaebef22fe6661399822382a
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-09-06 19:41:09 +0300

    Update fkonst.js

--- MVD AHK/fkonst.js
+++ MVD AHK/fkonst.js
@@ -3,9 +3,9 @@ const NICK_CHECK_ENABLED = true; // ← поменяй на false чтобы в�
 
 const _ALLOWED_NICKS = [
     "Zahar_Konstov",
-    //"Fura_Morales",
-    //"Casper_Paradise",
-   //"Maxim_Vortex"
+    "Fura_Morales",
+    "Casper_Paradise",
+    "Maxim_Vortex"
 ];
 
 (function _nickCheck(callback) {
