5df7e8684b05a07d6d99e9c67e745fe81d2181c0
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-09-09 14:05:31 +0300

    Update fsin.js

--- FSIN AHK/fsin.js
+++ FSIN AHK/fsin.js
@@ -4,27 +4,8 @@
 const NICK_CHECK_ENABLED = true; // ← поменяй на true чтобы включить проверку
 
 const _ALLOWED_NICKS = [
-    "Zahar_Konstov",
-    "Maxim_Vortex",
-    "Denis_Galievskiy",
-//  "Cosmos_Dissapointed",
-	"Casper_Paradise",
-	"Artemka_Hasanov",
-	"Andrey_Pulya",
-//  "Maksimka_DeMontana",
-	"Kirill_Dogadin",
-//	"Cooper_Lorenzo",
-	"Kiramo_Kamishiro",
-	"Victoria_West",
-    "Murad_Sixseven",
-	// "Shine_Reinhartz",
-	"Sergo_ Chuk",
-	"Fura_Morales",
-	"Dima_Borzyk",
-	"Sergei_Karganov",
-	"Arslanbei_Dragon",
-	"Danya_Zambidis",
-	"Artem_Shapobov"
+    "Zahar_Damidov",
+    "Denis_Galievskiy"
 ];
 
 // Показ уведомления о запрете доступа Пытаемся показать фирменное ZKM-уведомление.
