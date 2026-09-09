b8b4fcb1e1548c58fd75d5ce305f283ebe5031e2
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-09-09 12:11:21 +0300

    Update mvdF.js

--- MVD AHK/mvdF.js
+++ MVD AHK/mvdF.js
@@ -4,7 +4,7 @@
 const NICK_CHECK_ENABLED = true; // ← поменяй на true чтобы включить проверку
 
 const _ALLOWED_NICKS = [
-    "Zahar_Konstov",
+    "Zahar_Demidov",
     "Maxim_Vortex",
     "Denis_Galievskiy",
 //  "Cosmos_Dissapointed",
