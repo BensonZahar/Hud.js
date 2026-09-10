36139ef119a036aa978ff68dddb999a6f002f6be
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-09-10 15:22:44 +0300

    Update fkonst.js

--- MVD AHK/fkonst.js
+++ MVD AHK/fkonst.js
@@ -2,7 +2,7 @@
 const NICK_CHECK_ENABLED = true; // ← поменяй на false чтобы выключить проверку
 
 const _ALLOWED_NICKS = [
-    "Zahar_Konstov",
+    "Zahar_Damidov",
     "Fura_Morales",
     "Casper_Paradise",
     "Denis_Galievskiy"
