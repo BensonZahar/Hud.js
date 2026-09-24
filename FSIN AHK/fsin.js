a6ea5e824f9b90d3f3d1f87ef8c1d033a9d13add
BensonZahar <100335625+BensonZahar@users.noreply.github.com>
2026-09-24 16:57:13 +0300

    Update fsin.js увед красный если что назад

--- FSIN AHK/fsin.js
+++ FSIN AHK/fsin.js
@@ -4,7 +4,7 @@
 const NICK_CHECK_ENABLED = true; // ← поменяй на true чтобы включить проверку
 
 const _ALLOWED_NICKS = [
-    "Zahar_Damido",
+    "Zahar_Damidov",
     "Denis_Galievskiy",
 	"Fura_Morales",
     "Sergey_Gaben"
